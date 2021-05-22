979cb10fad74f43d5127f38fae773ca56975c52e
BAO-Jiale <84310779+BAO-Jiale@users.noreply.github.com>
2021-05-23 05:11:22 +0800

    Add files via upload

---
+++ EventListener.java
@@ -0,0 +1,13 @@
+import com.jme3.input.controls.ActionListener;
+
+public class EventListener implements ActionListener {
+    //通过射线检测判断操作的是哪个格子
+    public int[] getGrid(){return null;}
+
+    @Override
+    public void onAction(String s, boolean b, float v) {
+
+    }
+    //进行操作
+
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        MainFrame app = new MainFrame();
+        app.start();
+    }
+}
+
